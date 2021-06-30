6c88b6e920bd4f1c62c98fd185aee58aa002eb3d
ayeshan96 <ayeshandissanayaka1@gmail.com>
2021-06-30 11:40:37 +0530

    update discovery server

---
+++ discover-server/src/test/java/co4353distributedsystems/discoverserver/DiscoverServerApplicationTests.java
@@ -0,0 +1,13 @@
+package co4353distributedsystems.discoverserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoverServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

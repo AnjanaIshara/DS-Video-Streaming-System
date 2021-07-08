60f4867ce53d284084cca65cda10b7a277e8a6e1
anjana ishara <anjanaishara08@gmail.com>
2021-07-08 13:20:29 +0530

    minor changes

--- apigateway/src/main/java/co4353distributedsystems/apigateway/ApigatewayApplication.java
+++ apigateway/src/main/java/co4353distributedsystems/apigateway/ApigatewayApplication.java
@@ -3,11 +3,12 @@ package co4353distributedsystems.apigateway;
 import co4353distributedsystems.apigateway.routingandfiltergateway.SimpleFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
 
 
-
+@EnableDiscoveryClient
 @EnableZuulProxy
 @SpringBootApplication
 public class ApigatewayApplication {

864c4cd80041c24a300ed4f73e6aad841681cb74
Reggie509 <anjanaishara08@gmail.com>
2021-08-20 13:55:55 +0530

    added the data.sql file to categorized movoes

---
+++ categorized-movies/src/main/resources/data.sql
@@ -0,0 +1,2 @@
+INSERT INTO CATEGORIZED_MOVIES (CATEGORY,MOVIENAME,URL) values('comedy','Mr. Bean','www.google.com'),
+                                                               ('action','F9','www.imdb.com');

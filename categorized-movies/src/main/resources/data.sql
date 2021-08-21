a3ee41c279353c4afd507c8a2997ceba99af8223
Reggie509 <anjanaishara08@gmail.com>
2021-08-21 19:12:37 +0530

    added images to db

--- categorized-movies/src/main/resources/data.sql
+++ categorized-movies/src/main/resources/data.sql
@@ -1,6 +1,9 @@
-INSERT INTO CATEGORIZED_MOVIES (CATEGORY,MOVIENAME,URL) values('comedy','Mr. Bean','www.google.com'),
-                                                              ('comedy','Hang over','www.google.com'),
-                                                              ('comedy','Game Night','www.google.com'),
-                                                               ('action','F9','www.imdb.com'),
-                                                                ('action','The Dark Knight','www.imdb.com'),
-                                                                 ('action','Mad Max','www.imdb.com');
+INSERT INTO CATEGORIZED_MOVIES (CATEGORY,MOVIENAME,URL) values('comedy','Mr. Bean','1QQjZ68bTzKQE9xzou6Gxa1dd0-EeTt4k'),
+                                                              ('comedy','Hang over','1jNDJmYG0rJ1ZM2ZdXlQVTk8R4Uh1-kXw'),
+                                                              ('comedy','Game Night','1b1h-NaXXnqH0_q3BmZcRMUN455ECq9O3'),
+                                                               ('action','F9','1sghbJlLbb3zUG2mgAzyJDL24zSLbCZy9'),
+                                                                ('action','The Dark Knight','19TBVkNBPz0x3QLUmeTSKEsGcGwU6kRvQ'),
+                                                                 ('action','Mad Max','1DAVjpKTJIm7rpU09RWLYHU0fd_MocghR'),
+                                                               ('horror','The Silence','1Q-gwwC6L3zRxBILj2m0QKVvjT_e0w8rG'),
+                                                               ('horror','The Fantasy Island','1hZRYfPeenNXUCy1Jo2zgOloujqBt8d1_'),
+                                                               ('horror','The turing','1dQp9qxnHLKmnY9J0pCzKKCdoTyFBMftF');
